36e063695fb32bf88b7098492846fdb509f44351
Błażej Bartoszewski <blazejbart@gmail.com>
2024-05-17 11:57:26 +0200

    add categories

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const passport = require("./config/passport");
 
 const incomeRouter = require("./routes/incomeRoutes");
-const expenseRouter = require("./routes/expensesRoutes")
+const expenseRouter = require("./routes/expensesRoutes");
+const categoryRouter = require("./routes/categoryRoutes");
 const authRouter = require("./routes/authRouter");
 const authMid = require("./middleware/authMiddleware");
 const usersRouter = require("./routes/usersRouter");
@@ -21,6 +22,7 @@ app.use(passport.initialize());
 app.use("/api/auth", authRouter);
 app.use("/api/transaction/income", authMid, incomeRouter); // Zmiana ścieżki
 app.use("/api/transaction/expense", authMid, expenseRouter); // Zmiana ścieżki
+app.use("/api/transaction", authMid, categoryRouter); 
 app.use("/api/user", authMid, usersRouter);
 // dodatkowo przed routerem można dodać middleware autoryzujący użytkownika
 
