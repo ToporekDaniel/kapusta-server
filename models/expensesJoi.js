453b444ddcc7c926837f784dc51260a88b9b106a
ToporekDaniel <danieltoporek0@gmail.com>
2024-05-21 18:38:31 +0200

    poprawa

--- models/expensesJoi.js
+++ models/expensesJoi.js
@@ -1,10 +1,10 @@
-const Joi = require('@hapi/joi');
+const Joi = require("@hapi/joi");
 
 const expensesSchema = Joi.object({
   description: Joi.string().min(1).max(300).required(),
-  amount: Joi.number().min(1).max(1000000000).required(),
+  amount: Joi.number().min(-10000000).max(1000000000).required(),
   date: Joi.date().iso().required(),
-  category: Joi.string().required()
+  category: Joi.string().required(),
 });
 
 module.exports = expensesSchema;
