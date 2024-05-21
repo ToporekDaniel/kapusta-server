800c0b5c0a5380c8ee79c87b4ad7b08bb1eda648
ToporekDaniel <danieltoporek0@gmail.com>
2024-05-21 18:42:11 +0200

    up

--- models/incomeJoi.js
+++ models/incomeJoi.js
@@ -1,10 +1,11 @@
-const Joi = require('@hapi/joi');
+const Joi = require("@hapi/joi");
 
 // Definicja walidatora dla danych wejściowych
 const incomeSchema = Joi.object({
   description: Joi.string().min(1).max(300).required(),
   amount: Joi.number().min(1).max(1000000000).required(),
-  date: Joi.date().iso().required()
+  date: Joi.date().iso().required(),
+  category: Joi.string().required(),
 });
 
 module.exports = incomeSchema;
